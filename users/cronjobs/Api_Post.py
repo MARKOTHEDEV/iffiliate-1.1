67d688c7fa40def832a3f6380a47bdee861db3cd
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-08-08 21:21:24 -0700

    changed debug to False

---
+++ users/cronjobs/Api_Post.py
@@ -0,0 +1,88 @@
+import requests,json
+
+from requests.api import post
+from users import models
+
+
+
+
+
+
+class NewsApi:
+
+    def __init__(self,url,url2,number_of_post):
+        self.url = url
+        self.url2 = url2#this is just a back up url
+        self.number_of_post = number_of_post
+
+
+
+    def get_api_data(self):
+        'this get all the data or response from the internet'
+        posts = []
+        try:
+
+            resp = requests.get(url=url)
+            respData = resp.json()
+            posts.append(respData)
+            print(respData.get('totalResults'),'Post Number')
+            if respData.get('totalResults') >= self.number_of_post:
+                "so we saying if the total number is not up to the number of post just request again"
+                print(len(posts),'len of posts')
+                return posts
+            else:
+                # this means we did not get enough post the we have to re fecth
+                self.url = self.url2
+                self.get_api_data()
+        except requests.exceptions.ConnectionError:
+            print("trying again Because of Network Error")
+            self.get_api_data()
+            
+    def format_api_data(self,raw_data):
+        '''
+            this format the data for inserting to db also
+            reduce how may post we gonna see using the "self.number_of_post"
+        '''
+        main_post = []
+        """
+          the if below checks if we have two different resouce that means we used to diffrent urls which is
+          url and url2
+        """
+        
+        if len(raw_data) == 2:
+            main_post.extend(raw_data[0]['articles'])
+            main_post.extend(raw_data[1]['articles'])
+        else:
+            # print(raw_data)
+            main_post.extend(raw_data[0]['articles'])
+
+        
+        return main_post[0:self.number_of_post]
+
+    def save_to_db(self,clean_data):
+        'save arranged data to db'
+        for news in clean_data:
+            moneypost =models.MoneyPost.objects.create(title=news.get('title'),content=news.get('content'))
+            moneypost.save()
+    
+    def run(self):
+        """ 
+            Steps
+            get the data from the internet - using  self.get_api_data()
+            arrange  the data for the database -- using self.format_api_data()
+            then we save that data = using self.save_to_db()
+        """
+        unFormated_data =  self.get_api_data()
+        cleaned_data = self.format_api_data(unFormated_data)
+        print('leght finall data',len(cleaned_data))
+        self.save_to_db(cleaned_data)
+
+
+
+api_key ='24b5744b7c444e4aabc4d26213ee15e7'
+url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}'
+url2 = f'https://newsapi.org/v2/top-headlines?country=br&apiKey={api_key}'
+
+
+NEWS = NewsApi(url,url,10)
+NEWS.run()
--- users/cronjobs/cronjob.py
+++ users/cronjobs/cronjob.py
@@ -2,21 +2,22 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from users import models
 from .checkUserStatus import UserStatusChecker
 from .NewsGetterAndSaver import runScraper
+from .Api_Post import NEWS
 # from .chooseRaffleDraw_winner import chooseRaffleDraw_winner_randomly
 from . import payUser_onlogin
 
-def me():
-    moneypost =models.MoneyPost.objects.create(title="self.postTitle",content="self.postContent")
-    moneypost.save()
+
 def start():
     'this function start the task when it runs'
     scheduler = BackgroundScheduler()
     userChecker = UserStatusChecker()
+    
     print("Loading Scheduler.....")
     # scheduler.add_job(runScraper,"interval",hours=24,id="MoneyPost_001",replace_existing=True)
     # scheduler.add_job(userChecker.start,"interval",hours=23,id="checkExpiredSub_001",replace_existing=True)
     # scheduler.add_job(payUser_onlogin.start,"interval",hours=21,id="payUser_onlogin_001",replace_existing=True)
-    scheduler.add_job(me,"interval",minutes=20,id="payUser_onlogin_001",replace_existing=True)
+
+    scheduler.add_job(NEWS.run,"interval",minutes=2,id="payUser_onlogin_001",replace_existing=True)
     
     
     scheduler.start()
