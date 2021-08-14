305789dfb111403bd9f1c946670d9e40040c6405
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-08-13 20:49:55 -0600

    did some configuration for time zone

--- users/tests.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-# subscriptionExpireDateStr = datetime.datetime.strftime(subObject.expires_in,'%y-%m-%d')
-# # all i did was to parse the todayStr to a date object
-# userSubExpireDate = datetime.datetime.strptime(subscriptionExpireDateStr,'%y-%m-%d')
-
-a = {"2":2}
-
-print(a.get('1',"Not"))
