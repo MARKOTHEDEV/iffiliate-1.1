ab289001f2c41d8f4c43b14e1a20694ef44f19ef
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-08-26 14:19:04 -0600

    want to test heroku Server

--- users/cronjobs/cronjob.py
+++ users/cronjobs/cronjob.py
@@ -5,7 +5,11 @@ from .NewsGetterAndSaver import runScraper
 from .Api_Post import NEWS
 # from .chooseRaffleDraw_winner import chooseRaffleDraw_winner_randomly
 from . import payUser_onlogin
-
+from users.models import MoneyPost
+def create():
+    MoneyPost.objects.create(
+        title="a",content="a",image_link = "fjkdf"
+    )
 
 def start():
     'this function start the task when it runs'
@@ -13,13 +17,14 @@ def start():
     userChecker = UserStatusChecker()
     
     print("Loading Scheduler.....")
-    "every 19 hours  Check if the user Sub Has Expired"
-    scheduler.add_job(userChecker.start,"interval",hours=19,id="checkExpiredSub_001",replace_existing=True)
+    # "every 19 hours  Check if the user Sub Has Expired"
+    # scheduler.add_job(userChecker.start,"interval",hours=19,id="checkExpiredSub_001",replace_existing=True)
 
-    "every 23 hours  if user has logged in for that day and pay them"
-    scheduler.add_job(payUser_onlogin.start,"interval",hours=23,id="payUser_onlogin_001",replace_existing=True)
-    "every 24 hours  Get News Articles So USers can Earn"
-    scheduler.add_job(NEWS.run,"interval",minutes=1,id="get_money_post_002",replace_existing=True)
+    # "every 23 hours  if user has logged in for that day and pay them"
+    # scheduler.add_job(payUser_onlogin.start,"interval",hours=23,id="payUser_onlogin_001",replace_existing=True)
+    # "every 24 hours  Get News Articles So USers can Earn"
+    # scheduler.add_job(NEWS.run,"interval",minutes=1,id="get_money_post_002",replace_existing=True)
+    scheduler.add_job(create,"interval",minutes=1,id="get_money_post_002",replace_existing=True)
     
     
     scheduler.start()
